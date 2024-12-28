3c64b832efa573ec7e5d67dfc81de535953b72f7
Alex <avlonitis@msn.com>
2024-12-28 19:50:57 +0200

    Small refactor
    remove the multiple command executions

--- commands/command_parser.go
+++ commands/command_parser.go
@@ -16,32 +16,31 @@ func CommandExecute(cmd Command) {
 
 // ParseCommand parses a command and sends the appropriate IRC command
 func SendCommand(input string, client *connection.Client) {
+	var cmd Command
+
 	if strings.HasPrefix(input, "/") {
 		parts := strings.Split(input, " ")
 
 		switch parts[0] {
 		case "/join":
-			cmd := &JoinCommand{Input: input, Client: client}
-			CommandExecute(cmd)
+			cmd = &JoinCommand{Input: input, Client: client}
 		case "/part":
-			cmd := &PartCommand{Input: input, Client: client}
-			CommandExecute(cmd)
+			cmd = &PartCommand{Input: input, Client: client}
 		case "/nick":
-			cmd := &NickCommand{Input: input, Client: client}
-			CommandExecute(cmd)
+			cmd = &NickCommand{Input: input, Client: client}
 		case "/quit":
-			cmd := &QuitCommand{Input: input, Client: client}
-			CommandExecute(cmd)
+			cmd = &QuitCommand{Input: input, Client: client}
 		case "/help":
-			cmd := &HelpCommand{}
-			CommandExecute(cmd)
+			cmd = &HelpCommand{}
 		default:
 			fmt.Println("\nInvalid command, use /help for more commands")
+			return
 		}
 
+		CommandExecute(cmd)
 		return
 	}
 
-	cmd := &MessageCommand{Input: input, Client: client}
+	cmd = &MessageCommand{Input: input, Client: client}
 	CommandExecute(cmd)
 }
