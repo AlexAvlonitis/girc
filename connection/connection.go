836993087011035d713b207759edd73463ca89b5
Alex <avlonitis@msn.com>
2025-01-05 15:59:11 +0200

    Refactor with gpt`

--- connection/connection.go
+++ connection/connection.go
@@ -17,28 +17,36 @@ type DefaultConnection struct {
 func (c *DefaultConnection) Conn() net.Conn { return c.conn }
 
 // NewConnection creates a new connection to the server
-func NewConnection(c interfaces.Client) interfaces.Connection {
+func NewConnection(client interfaces.Client) interfaces.Connection {
+	address := client.Server() + ":" + strconv.Itoa(client.Port())
 	var conn net.Conn
 	var err error
 
-	if c.Ssl() {
-		fmt.Println("Connecting to server over SSL...")
-
-		tlsConfig := &tls.Config{
-			InsecureSkipVerify: true, // Skip certificate verification (unsafe for production)
-		}
-
-		conn, err = tls.Dial("tcp4", c.Server()+":"+strconv.Itoa(c.Port()), tlsConfig)
-		if err != nil {
-			log.Fatalf("Error connecting to server: %s", err)
-		}
+	if client.Ssl() {
+		conn, err = createSSLConnection(address)
 	} else {
-		fmt.Println("Connecting to server...")
-		conn, err = net.Dial("tcp4", c.Server()+":"+strconv.Itoa(c.Port()))
-		if err != nil {
-			log.Fatalf("Error connecting to server: %s", err)
-		}
+		conn, err = createTCPConnection(address)
+	}
+
+	if err != nil {
+		log.Fatalf("Error connecting to server: %s", err)
 	}
 
 	return &DefaultConnection{conn: conn}
+}
+
+func createSSLConnection(address string) (net.Conn, error) {
+	fmt.Println("Connecting to server over SSL...")
+
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: true, // Skip certificate verification (unsafe for production)
+	}
+
+	return tls.Dial("tcp4", address, tlsConfig)
+}
+
+func createTCPConnection(address string) (net.Conn, error) {
+	fmt.Println("Connecting to server...")
+
+	return net.Dial("tcp4", address)
 }
--- connection/message_parser.go
+++ connection/message_parser.go
@@ -7,6 +7,15 @@ import (
 	"strings"
 )
 
+const (
+	privMsg  = "PRIVMSG"
+	nickMsg  = "NICK"
+	namesMsg = "353"
+	pingMsg  = "PING"
+	joinMsg  = "JOIN"
+	partMsg  = "PART"
+)
+
 type Message struct {
 	Source  string
 	Command string
@@ -29,36 +38,31 @@ func (p *MessageParser) Parse(msg string) string {
 		return ""
 	}
 
-	if p.isPrivateMessage(message) {
-		return p.formatPrivateMsg(message)
-	} else if p.isNickChange(message) {
-		return p.formatNickChange(message)
-	} else if p.isJoin(message) {
-		return p.formatJoin(message)
-	} else if p.isPart(message) {
-		return p.formatPart(message)
-	} else if p.isNames(message) {
-		return p.formatNames(message)
-	} else if p.isPing(message) {
-		p.Client.Write("PONG " + p.formatPing(msg)) // keep the connection alive
-		return ""
+	handlers := map[string]func(*Message) string{
+		privMsg:  p.formatPrivateMsg,
+		nickMsg:  p.formatNickChange,
+		namesMsg: p.formatNames,
+		pingMsg:  p.handlePing,
+		joinMsg:  p.formatJoin,
+		partMsg:  p.formatPart,
+	}
+
+	if handler, exists := handlers[message.Command]; exists {
+		return handler(message)
 	}
 
 	return message.printMessage()
 }
 
-func (p *MessageParser) NamesToList(msg string) []string {
-	return strings.Split(msg, " ")
-}
-
-// format ping messages, get the message after the PING
-func (p *MessageParser) formatPing(msg string) string {
-	return msg[5:]
+// handlePing handles PING messages from the server, and sends a PONG message back
+// to keep the connection alive
+func (p *MessageParser) handlePing(msg *Message) string {
+	p.Client.SendPong(msg.Args[0])
+	return ""
 }
 
 // FormatNames parse the names of the users in a channel
 func (p *MessageParser) formatNames(msg *Message) string {
-	// get names after 4th index
 	names := msg.Args[3]
 	for _, name := range strings.Split(names, " ") {
 		p.Client.SetUsers(append(p.Client.Users(), name))
@@ -67,30 +71,6 @@ func (p *MessageParser) formatNames(msg *Message) string {
 	p.Ui.App.Draw()
 
 	return ""
-}
-
-func (p *MessageParser) isNames(msg *Message) bool {
-	return msg.Command == "353"
-}
-
-func (p *MessageParser) isPrivateMessage(msg *Message) bool {
-	return msg.Command == "PRIVMSG"
-}
-
-func (p *MessageParser) isNickChange(msg *Message) bool {
-	return msg.Command == "NICK"
-}
-
-func (p *MessageParser) isPing(msg *Message) bool {
-	return msg.Command == "PING"
-}
-
-func (p *MessageParser) isJoin(msg *Message) bool {
-	return msg.Command == "JOIN"
-}
-
-func (p *MessageParser) isPart(msg *Message) bool {
-	return msg.Command == "PART"
 }
 
 func (p *MessageParser) formatNickChange(msg *Message) string {
@@ -129,29 +109,39 @@ func parseMsg(s string) (*Message, error) {
 	var trailing string
 	var args []string
 
+	// Check if the input string is empty
 	if s == "" {
 		return nil, errors.New("empty string")
 	}
 
+	// If the message starts with a ':', it has a prefix
 	if s[0] == ':' {
+		// Split the string into prefix and the rest of the message
 		split := strings.SplitN(s[1:], " ", 2)
 		prefix = split[0]
 		s = split[1]
 	}
 
+	// Check if the message contains a trailing part after ' :'
 	if strings.Contains(s, " :") {
+		// Split the string into the main part and the trailing part
 		split := strings.SplitN(s, " :", 2)
 		s = split[0]
 		trailing = split[1]
+		// Split the main part into arguments and append the trailing part as the last argument
 		args = strings.Split(s, " ")
 		args = append(args, trailing)
 	} else {
+		// If there is no trailing part, split the entire string into arguments
 		args = strings.Split(s, " ")
 	}
 
+	// The first argument is the command
 	command := args[0]
+	// The rest are the arguments
 	args = args[1:]
 
+	// Return the parsed message
 	return &Message{
 		Source:  prefix,
 		Command: command,
